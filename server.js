72e2bc077aac39fa1c4a596ad2bb17e70e9c6424
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-08-06 17:07:11 -0500

    added api require index

--- server.js
+++ server.js
@@ -17,16 +17,16 @@ app.use(express.static('public'));
 
 // GET Route for homepage
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 // GET Route for feedback page
 app.get('/feedback', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
+    res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
 );
 
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/pages/404.html')));
 
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
