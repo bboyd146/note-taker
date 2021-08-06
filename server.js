100499e0e0b04eca0c1d577a269d1fe6233555d7
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-08-06 17:54:56 -0500

    get routes added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
-const api = require('./public/index.html');
+// const api = require('./public/index.html');
+const notesData = require('./db/db.json');
 
 const PORT = process.env.port || 3001;
 
@@ -10,21 +11,23 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-
 app.use(express.static('public'));
 
 // GET Route for homepage
-app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+// app.get('/', (req, res) =>
+//     res.sendFile(path.join(__dirname, '/public/index.html'))
+// );
 
 // GET Route for feedback page
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+app.get('/api/notes', (req, res) => {
+    res.json(notesData);
+    });
 
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/pages/404.html')));
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT} 🚀`)
